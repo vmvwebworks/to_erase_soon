4a118c33335bfa0391e2c4ad9ce04e45f691af3f
Marcos <vmvwebworks@gmail.com>
2018-01-26 00:35:07 +0100

    generate zombies controller and rspec tests

---
+++ app/controllers/zombie_controller.rb
@@ -0,0 +1,2 @@
+class ZombieController < ApplicationController
+end
---
+++ spec/controllers/zombie_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe ZombieController, type: :controller do
+  it "GET response for the API with a list of zombies in JSON" do
+
+  end
+  it "POST response for zombies when the API updates a zombie" do
+    
+  end
+end
