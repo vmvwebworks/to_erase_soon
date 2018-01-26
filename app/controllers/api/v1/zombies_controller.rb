38f10c59b94b579ee387dced67ce2892cd17030b
Marcos <vmvwebworks@gmail.com>
2018-01-26 23:12:17 +0100

    index & show methods for zombies

--- app/controllers/api/v1/zombies_controller.rb
+++ app/controllers/api/v1/zombies_controller.rb
@@ -5,18 +5,27 @@ module Api
         @zombies = Zombie.all
         json_response(@zombies)
       end
-      def create
-
-      end
       def show
-
+        @zombie = Zombie.find(params[:id])
+        json_response(@zombie)
+      end
+      def create
+        @zombie = Zombie.create!(item_params)
+        json_response(@zombie, :created)
       end
       def update
-
+        
       end
       def destroy
 
       end
+
+      private
+
+      def item_params
+        params.permit(:name, :done)
+      end
+
     end
   end
 end
